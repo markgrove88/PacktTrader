8d8fb2c3a1b5cb29004b3bcf648709f3c4579ce1
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-25 16:57:07 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='packtTrader',
-      version='0.0.8',
+      version='0.0.9',
       install_requires=['gym', 'numpy', 'pandas', 'matplotlib', 'yfinance', 'finta'],
       description='',
       url='',
