7b9abba1805d4ed2b92a1da99fa2052708aae4f7
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-25 09:33:42 +0100

    Corrected syntax

--- packt_trader/__init__.py
+++ packt_trader/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 register(
     id='packttrade-v0',
     entry_point='packt_trader.envs:TradingEnvironment',
-    max_episode_steps=252)
+    max_episode_steps=252
 )
