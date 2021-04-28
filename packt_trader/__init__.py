8996fc83c7399d3b0d9c09fcf347ba52cea0de05
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-28 11:50:31 +0100

    registered new trading id

--- packt_trader/__init__.py
+++ packt_trader/__init__.py
@@ -5,3 +5,9 @@ register(
     entry_point='packt_trader.envs:TradingEnvironment',
     max_episode_steps=252
 )
+
+register(
+    id='newtrader-v0',
+    entry_point='packt_trader.envs:NewTradingEnvironment',
+    max_episode_steps=252
+)
